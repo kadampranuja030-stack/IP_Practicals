df49d88ef1a6387fe157bf4e1dd2b6f986446b4e
Pranuja kadam <kadampranuja030@gmail.com>
2026-07-22 10:40:52 +0530

    Add files via upload

---
+++ prac_3Q1.py
@@ -0,0 +1,29 @@
+#Question 1
+num=int(input("Enter a number: "))
+
+if num>0:
+    print("The number is positive.")
+#Question 2
+num=int(input("Enter a number: "))
+
+if num%2==0:
+    print("Number is Even.")
+else:
+    print("Number is odd.")
+
+#Question 3
+marks=float(input("Enter the marks: "))
+
+if marks>=90:
+    print("Grade: A")
+elif marks>=75:
+    print("Grade: B")
+elif marks>=50:
+    print("Grade: C")
+else:
+    print("Fail")
+    
+
+
+
+    
