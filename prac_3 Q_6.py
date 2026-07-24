c634803572718c033ddef90c062ae85d9f0cc28f
Pranuja kadam <kadampranuja030@gmail.com>
2026-07-24 19:07:33 +0530

    Add files via upload

---
+++ prac_3 Q_6.py
@@ -0,0 +1,9 @@
+#Q_6 program to check the entered year is a leap year
+
+year=int(input("Enter the year:"))
+
+if(year%400==0) or (year%4==0 and year%100 != 0):
+    print(year,"is a leap year")
+else:
+    print(year,"is not a leap year")
+    
